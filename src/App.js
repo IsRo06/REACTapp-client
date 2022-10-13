a37e57e2becb7009c77d67cadbfa80c37d470a25
Isabella Roman <Isabella Roman>
2022-10-13 00:54:30 -0400

    edited app,js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Navigate as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
