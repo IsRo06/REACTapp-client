7b54132cae14f2e7747baa3f29e5f3441cc941ae
Isabella Roman <Isabella Roman>
2022-10-12 23:49:49 -0400

    proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://frozen-everglades-41178.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
