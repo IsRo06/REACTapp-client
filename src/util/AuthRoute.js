c02d329e6ccf85634db2245cf060761fd9d5c24e
Isabella Roman <Isabella Roman>
2022-10-13 00:58:01 -0400

    Updated outdated dependencies

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 
 import { AuthContext } from '../context/auth';
 
