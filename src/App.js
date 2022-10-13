48cfab9fddcfee041261c6e99ab562f2fcbaf3d5
Isabella Roman <Isabella Roman>
2022-10-13 01:20:31 -0400

    updated to correct version and fixed minor errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
